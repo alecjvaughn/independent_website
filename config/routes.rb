d4fd07f85762afc5af70979bbeb02fffa28f41b8
Alec Vaughn <alecj.vaughn@gmail.com>
2018-09-05 22:27:22 -0500

    Add contacts controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :contacts do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
